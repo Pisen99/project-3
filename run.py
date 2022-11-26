75ee16c31c180fdc614e958dcdb00036ecae9124
Pisen99 <patricia.andersson99@hotmail.com>
2022-11-26 18:56:37 +0000

    Feat: Used single-line & docstring to comment out rules and structure the game

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# ---------------------------- Battleship ----------------------------
+"""
+Rules:
+1. Contains a 5x5 grid with 5 ships randomly placed in the grid.
+2. Player will have 10 bullets to hit the ships.
+3. Player would choose a row & a column (1-5 & A-E) to aim their shot.
+4. Every missed or hit ship will show in the grid.
+5. Player wins if they shot all ships down before running out of bullets,
+   or else you will lose.
+
+Game Symbols:
+" " = Empty space
+- = Missed shot
+X = Ship's been shot.
+
+          A   B   C   D   E
+grid =  1   |   |   |   |   |
+        2   |   |   |   |   |
+        3   |   |   |   |   |
+        4   |   |   |   |   |
+        5   |   |   |   |   |
+"""
+# ---------------------------- INTRO ----------------------------
