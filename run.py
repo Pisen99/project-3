0fd46c54f30ea7470088cd6a1509e41c7971911d
Pisen99 <patricia.andersson99@hotmail.com>
2022-11-26 19:53:09 +0000

    Feat: Created a function using a while loop to display rules or start the game. Created a main function to run all functions written in it. Added some styling to print, easier read rules that way.

--- run.py
+++ run.py
@@ -48,5 +48,53 @@ def get_player_name():
         break
     return name
 
-player_name = get_player_name()
-print(f"Alright {player_name}, let's get started!\n")
+
+# ---------------------------- SELECT RULES OR PLAY GAME ----------------------------
+def rules_option():
+    """
+    Creating an input, giving user 2 options to display rules or play game with a yes or no answer.
+    Validates input if it violates certan criterias. (answer must be 'yes' or 'no'),
+    if it does, throw an error message and go back to the top.
+    Some basic styling to 'print' to make it easier for user to read in the terminal.
+    """
+    while True:
+        rules = input("Would you like to display the rules? please enter 'yes' or 'no'\n").strip()
+        # Rules will display if player enter "yes".
+        if rules == 'yes':
+            print("------------------------------------------------------")
+            print("")
+            print("Rules:\n1. Contains a 5x5 grid with 5 ships randomly placed in the grid.\n2. Player will have 10 bullets to hit the ships.\n3. Player would choose a row & a column (1-5 & A-E) to aim their shot.\n4. Every missed or hit ship will show in the grid.\n5. Player wins if they shot all ships down before running out of bullets, or else you will lose.")
+            print("")
+            print("Game Symbols:\n"  " = Empty space\n- = Missed shot\nX = Ship's been shot.")
+            print("")
+            print("------------------------------------------------------")
+            continue
+
+        # Game will start if player enter "no".
+        if rules == 'no':
+            print("------------------------------------------------------")
+            print("")
+            print("Game starts now..")
+            print("")
+            print("------------------------------------------------------")
+            continue
+
+        else:
+            print("Please enter 'yes' or 'no'\n")
+            continue
+        break
+    return rules
+
+
+# ---------------------------- MAIN FUNCTION ----------------------------
+
+def main():
+    """
+    Run all functions data
+    """
+    player_name = get_player_name()
+    print(f"Alright {player_name}, let's get started!\n")
+
+    rules_answer = rules_option()
+
+main()
